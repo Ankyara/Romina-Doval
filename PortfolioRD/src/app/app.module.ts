c1d19a54140548cb07200c6c9e32d9b6464b9120
Ankyara <romina.doval@gmail.com>
2022-08-03 20:25:05 -0300

    todo pasado

--- PortfolioRD/src/app/app.module.ts
+++ PortfolioRD/src/app/app.module.ts
@@ -4,11 +4,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { RedesNavComponent } from './components/redes-nav/redes-nav.component';
+import { DividerComponent } from './components/divider/divider.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AboutComponent } from './components/about/about.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { EducationComponent } from './components/education/education.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FooterNavComponent } from './components/footer-nav/footer-nav.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LogoAPComponent,
+    RedesNavComponent,
+    DividerComponent,
+    BannerComponent,
+    AboutComponent,
+    SkillsComponent,
+    EducationComponent,
+    ProyectosComponent,
+    FooterComponent,
+    FooterNavComponent,
   ],
   imports: [
     BrowserModule,
---
+++ PortfolioRD/src/app/components/redes-nav/redes-nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-redes-nav',
+  templateUrl: './redes-nav.component.html',
+  styleUrls: ['./redes-nav.component.css']
+})
+export class RedesNavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
